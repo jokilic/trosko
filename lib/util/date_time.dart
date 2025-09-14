dea0d33c9e7b6e9c0660c1fc3d912c6c52adf391
Josip Kilić <neksuses@gmail.com>
2025-09-14 17:16:32 +0200

    Remove unnecessary dependencies

--- lib/util/date_time.dart
+++ lib/util/date_time.dart
@@ -1,11 +1,4 @@
 import 'package:intl/intl.dart';
-import 'package:timeago/timeago.dart' as timeago;
-
-String getTimeAgo(DateTime dateTime) => timeago.format(
-  dateTime,
-  locale: 'hr',
-  allowFromNow: true,
-);
 
 String getFormattedDate(DateTime dateTime) => DateFormat('dd. MMMM yyyy', 'hr').format(dateTime);
 
