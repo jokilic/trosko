ff097117aec7b07db13beb2d46d3ee907b629432
Josip Kilić <neksuses@gmail.com>
2025-09-29 00:15:40 +0200

    New Web build again

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "63cb9e092c6b270251355436b7d2b6f9",
+const RESOURCES = {"flutter_bootstrap.js": "86d5f7b4a1fada2ecc3f02b1ecea0d7b",
 "version.json": "209bdbc423dce7c4da2e16b653f2df31",
 "splash/img/light-background.png": "8611ce5984357365177caf13e07ee31c",
 "splash/img/dark-background.png": "abe776f9dc234977032c9da3d9ed68b4",
@@ -12,13 +12,11 @@ const RESOURCES = {"flutter_bootstrap.js": "63cb9e092c6b270251355436b7d2b6f9",
 "main.dart.js": "3c3cbac005719ef10be1bfc3cd657dbc",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "1fda358f339dbb5a0279530489553901",
-"main.dart.mjs": "7a0d1b89954f0bf573e5a04b8bdec137",
 "icons/Icon-192.png": "550e9758eae452d0868bec0e14be6cd3",
 "icons/Icon-maskable-192.png": "550e9758eae452d0868bec0e14be6cd3",
 "icons/Icon-maskable-512.png": "68a8cd10ae18d0209970eb96c7111b77",
 "icons/Icon-512.png": "68a8cd10ae18d0209970eb96c7111b77",
 "manifest.json": "8a7f255a8d53075f86c688f429bb88cd",
-"main.dart.wasm": "70e6ba0e75f81eff66ef2e6ec7887229",
 "assets/AssetManifest.json": "70814fb4b99e182f619c90a25d65e0d1",
 "assets/NOTICES": "2aa50368d54e7ae61a7ced90287ce10c",
 "assets/FontManifest.json": "42d75805c38af5c779951dcb42ed9e92",
@@ -68,8 +66,6 @@ const RESOURCES = {"flutter_bootstrap.js": "63cb9e092c6b270251355436b7d2b6f9",
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
-"main.dart.wasm",
-"main.dart.mjs",
 "index.html",
 "flutter_bootstrap.js",
 "assets/AssetManifest.bin.json",
